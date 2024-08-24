71cb0c9aa71074c606218c2e64417c427dc09aae
bangchand <chandrakusumaa133@gmail.com>
2024-08-25 00:39:04 +0700

    fix route

--- routes/chandrak.php
+++ routes/chandrak.php
@@ -8,6 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::resource('attendance', AttendanceController::class);
     Route::resource('employee', EmployeeDetailsController::class);
+    Route::get('/mark-absentees', [AttendanceController::class, 'markAbsentees']);
 });
-
-Route::get('/mark-absentees', [AttendanceController::class, 'markAbsentees']);
