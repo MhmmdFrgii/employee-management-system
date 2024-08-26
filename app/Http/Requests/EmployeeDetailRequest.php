00d79981563d7e8e1853c424d8f2f135b17c317c
bangchand <chandrakusumaa133@gmail.com>
2024-08-26 10:36:33 +0700

    add some new column to employee model

--- app/Http/Requests/EmployeeDetailRequest.php
+++ app/Http/Requests/EmployeeDetailRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class EmployeeDetailRequest extends FormRequest
 {
@@ -25,7 +26,12 @@ class EmployeeDetailRequest extends FormRequest
             'user_id' => ['required', 'exists:users,id'],
             'department_id' => ['required', 'exists:departments,id'],
             'position_id' => ['required', 'exists:positions,id'],
-            'phone' => ['required', 'numeric'],
+            'nik' => ['required', 'string', 'min_digits:16', 'max_digits:16', Rule::unique('employee_details', 'nik')->ignore($this->employee)],
+            'fullname' => ['required', 'string'],
+            'photo' => ['required', 'mimes:png,jpg', 'max:2048'],
+            'cv' => ['required', 'mimes:png,jpg', 'max:2048'],
+            'phone' => ['required', 'numeric', Rule::unique('employee_details', 'phone')->ignore($this->employee)],
+            'gender' => ['required', 'string'],
             'address' => ['required', 'string'],
             'hire_date' => ['required', 'date'],
         ];
