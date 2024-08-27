0c10d0a64e6268de8992ef1d4ad25df7e8645be1
MhmmdFrgii <132626734+MhmmdFrgii@users.noreply.github.com>
2024-08-27 13:04:35 +0700

    New blade user Karywan

--- routes/firgi.php
+++ routes/firgi.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmployeeDetailsController;
 use App\Http\Controllers\KanbanBoardController;
 use App\Http\Controllers\KanbanTasksController;
 use Illuminate\Support\Facades\Route;
@@ -7,4 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::resource('kanbanboard', KanbanBoardController::class);
     Route::resource('kanbantasks', KanbanTasksController::class);
+
+    Route::get('/userKaryawan', [EmployeeDetailsController::class, 'userKaryawan']);
+
 });
