7639c66a3cbc2a3804bf1918dc43e80348580dd1
bangchand <chandrakusumaa133@gmail.com>
2024-08-29 18:10:47 +0700

    refactoring model file

--- app/Models/KanbanTask.php
+++ app/Models/KanbanTask.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class KanbanTasks extends Model
+class KanbanTask extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description', 'kanban_boards_id', 'status', 'date', 'color', 'employee_id'];
