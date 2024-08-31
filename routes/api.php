c80a3dc0bad7da8158d5f2f6e7c67914b9341730
IqbalFirmans <iqbalclan74@gmail.com>
2024-09-01 01:55:20 +0700

    add create project api for manager

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Whoops\Run;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,10 +12,13 @@ Route::get('/user', function (Request $request) {
 
 
 Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login'])->name('login');
-    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-
+    Route::middleware('role:manager')->group(function () {
+        Route::get('projects', [ProjectController::class, 'getAllProject']);
+        Route::post('project', [ProjectController::class, 'createProject']);
+    });
 });
