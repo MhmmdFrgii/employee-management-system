207ddc290e678b84d986dd78ba91250c41203a3d
IqbalFirmans <iqbalclan74@gmail.com>
2024-08-31 19:19:21 +0700

    create login and logout

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+
+});
