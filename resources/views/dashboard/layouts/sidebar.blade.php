37b08af518fa18b946e54655ac8deba214d78db2
bangchand <chandrakusumaa133@gmail.com>
2024-09-08 22:08:29 +0700

    mengatur selected menu saat query

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -26,7 +26,7 @@
                 <!-- Dashboard -->
                 <!-- =================== -->
 
-                <li class="sidebar-item">
+                <li class="sidebar-item {{ Request::is('manager/dashboard') ? 'selected' : '' }}">
                     <a class="sidebar-link" href="{{ route('manager.dashboard') }}" aria-expanded="false">
                         <span>
                             <i class="ti ti-aperture"></i>
@@ -34,7 +34,8 @@
                         <span class="hide-menu">Dasbor</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
+
+                <li class="sidebar-item {{ Request::is('manager/attendance*') ? 'selected' : '' }}">
                     <a class="sidebar-link" href="{{ route('attendance.index') }}" aria-expanded="false">
                         <span>
                             <i class="ti ti-clipboard-data"></i>
@@ -42,7 +43,8 @@
                         <span class="hide-menu">Absensi</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
+
+                <li class="sidebar-item {{ Request::is('manager/projects*') ? 'selected' : '' }}">
                     <a class="sidebar-link" href="{{ route('projects.index') }}" aria-expanded="false">
                         <span class="d-flex">
                             <i class="ti ti-clipboard-list"></i>
@@ -50,7 +52,8 @@
                         <span class="hide-menu">Proyek</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
+
+                <li class="sidebar-item {{ Request::is('manager/positions*') ? 'selected' : '' }}">
                     <a class="sidebar-link" href="{{ route('positions.index') }}" aria-expanded="false">
                         <span>
                             <i class="ti ti-list-details"></i>
@@ -58,7 +61,8 @@
                         <span class="hide-menu">Jabatan</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
+
+                <li class="sidebar-item {{ Request::is('manager/departments*') ? 'selected' : '' }}">
                     <a class="sidebar-link" href="{{ route('departments.index') }}" aria-expanded="false">
                         <span>
                             <i class="ti ti-category-2"></i>
@@ -66,7 +70,8 @@
                         <span class="hide-menu">Departemen</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
+
+                <li class="sidebar-item {{ Request::is('manager/employees*') ? 'selected' : '' }}">
                     <a class="sidebar-link" href="{{ route('employees.index') }}" aria-expanded="false">
                         <span>
                             <i class="ti ti-users"></i>
@@ -74,31 +79,26 @@
                         <span class="hide-menu">Karyawan</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="{{ route('invited.index') }}" aria-expanded="false">
-                        <span>
-                            <i class="ti ti-user-plus"></i>
-                        </span>
-                        <span class="hide-menu">Undangan</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="{{ route('applicants.index') }}" aria-expanded="false">
+
+                <li class="sidebar-item {{ Request::is('manager/candidates*') ? 'selected' : '' }}">
+                    <a class="sidebar-link" href="{{ route('candidates.index') }}" aria-expanded="false">
                         <span>
                             <i class="ti ti-user-up"></i>
                         </span>
                         <span class="hide-menu">Pelamar</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
+
+                <li class="sidebar-item {{ Request::is('manager/salaries*') ? 'selected' : '' }}">
                     <a href="{{ route('salaries.index') }}" class="sidebar-link">
                         <div class="round-16 d-flex align-items-center justify-content-center">
                             <i class="ti ti-coins"></i>
                         </div>
-                        <span class="hide-menu">Gaji </span>
+                        <span class="hide-menu">Gaji</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
+
+                <li class="sidebar-item {{ Request::is('manager/leave-requests*') ? 'selected' : '' }}">
                     <a href="{{ route('leave-requests.index') }}" class="sidebar-link">
                         <div class="round-16 d-flex align-items-center justify-content-center">
                             <i class="ti ti-license"></i>
@@ -106,14 +106,15 @@
                         <span class="hide-menu">Permintaan Cuti</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
+
+                <li class="sidebar-item {{ Request::is('notifications*') ? 'selected' : '' }}">
                     <a href="{{ route('notifications.index') }}" class="sidebar-link d-flex align-items-center">
                         <span>
                             <i class="ti ti-inbox"></i>
                         </span>
                         <span class="hide-menu">Kotak Masuk</span>
                         @if (isset($newNotificationCount) && $newNotificationCount > 0)
-                            <span class="badge bg-danger text-sm  rounded-pill ms-auto p-1">
+                            <span class="badge bg-danger text-sm rounded-pill ms-auto p-1">
                                 {{ $newNotificationCount }}
                                 <span class="visually-hidden">unread messages</span>
                             </span>
