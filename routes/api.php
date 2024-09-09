ee7f60f3b2d1541f723a8d7459420d57d94bcee7
bangchand <chandrakusumaa133@gmail.com>
2024-09-09 22:47:38 +0700

    make necessary route for api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AttendanceController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\KanbanController;
+use App\Http\Controllers\Api\LeaveRequestController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,8 +20,18 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::middleware('role:manager')->group(function () {
+    Route::middleware('role:employee')->group(function () {
         Route::get('projects', [ProjectController::class, 'getAllProject']);
-        Route::post('project', [ProjectController::class, 'createProject']);
+
+        Route::get('kanban-board', [KanbanController::class, 'index']);
+        Route::post('kanban-task', [KanbanController::class, 'storeTask']);
+        Route::put('/kanban-task/{id}', [KanbanController::class, 'updateTask']);
+        Route::delete('/kanban-task/{id}', [KanbanController::class, 'destroyTask']);
+
+        route::get('user-attendance', [AttendanceController::class, 'userIndex']);
+        route::post('user-attendance', [AttendanceController::class, 'userAttendance']);
+
+        route::get('leave-request', [LeaveRequestController::class, 'index']);
+        route::post('leave-request', [LeaveRequestController::class, 'store']);
     });
 });
